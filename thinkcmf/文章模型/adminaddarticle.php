8bf6c9212f8988ba8e94e332c9696a03d9103a49
aa3212689 <1064404481@qq.com>
2018-09-10 15:42:32 +0800

    勉为其难地存储到了数据
    最后试试前端解决

---
+++ thinkcmf/文章模型/adminaddarticle.php
@@ -0,0 +1,17 @@
+//        $abw=$data['awards'];
+//        dump($abw);
+//数据奖品数据插入
+//        $aw=array();
+//        $aw=$data["awards"];
+//        $data["awards"]['lai']='来吧';
+//$data["awards"]['you']="你好";
+//        $data["awards"]['me']="我们";
+
+//        $a3[]=explode("&amp;",$data['awards']);
+//        $data["awards"][0]=$a3[0];
+//        $data["awards"][1]=$a3[1];
+//        $data["awards"][2]=$a3[2];
+//        if (!empty($data['awards']) ) {
+//               $a=explode("&amp;",$data['awards']);
+//               $data['awards']=$a;
+//        }
